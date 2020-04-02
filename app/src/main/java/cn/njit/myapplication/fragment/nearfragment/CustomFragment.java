0f88d3d7842e7de3ad559720473f059d6875791c
we17 <3377417311@qq.com>
2020-04-02 15:54:44 +0800

    app 项目0.4.2版本
    成功添加sdk但需要根据设计添加新的功能
    
    新增任务
    *添加地图定位功能
    *添加地图查找周边场景功能
    *添加地图导航规划路线功能
    *添加地图查看实时路况功能

--- app/src/main/java/cn/njit/myapplication/fragment/nearfragment/CustomFragment.java
+++ app/src/main/java/cn/njit/myapplication/fragment/nearfragment/CustomFragment.java
@@ -3,27 +3,26 @@ package cn.njit.myapplication.fragment.nearfragment;
 import android.util.Log;
 import android.view.View;
 
-import com.amap.api.maps2d.AMap;
-import com.amap.api.maps2d.MapView;
+
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.MapView;
 
 import cn.njit.myapplication.R;
 import cn.njit.myapplication.fragment.BaseFragment;
 
 
 public class CustomFragment extends BaseFragment {
-
-
+    MapView mapView;
+    AMap aMap;
     private static final String TAG = CustomFragment.class.getSimpleName();//"CommonFrameFragment"
 
     @Override
     protected View initView() {
         Log.e(TAG, "自定义Fragment页面被初始化了...");
         View view = View.inflate(mContext, R.layout.fragment_custom_fragment, null);
-        MapView mapView = view.findViewById(R.id.map);//找到地图控件
-//在activity执行onCreate时执行mMapView.onCreate(savedInstanceState)，创建地图
+        mapView = view.findViewById(R.id.mapView);//找到地图控件
         mapView.onCreate(getArguments());
-        AMap aMap = mapView.getMap();//初始化地图控制器对象
-
+        aMap = mapView.getMap();//初始化地图控制器对象
         return view;
     }
 
