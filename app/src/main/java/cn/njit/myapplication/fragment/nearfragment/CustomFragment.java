cbeadddc4beedbbe00863f5b9ce308c3d6aeec73
we17 <3377417311@qq.com>
2020-04-02 15:03:04 +0800

    app 项目0.4.1版本
    成功添加sdk但需要根据设计添加新的功能
    
    新增任务
    *添加地图sdk插件

--- app/src/main/java/cn/njit/myapplication/fragment/nearfragment/CustomFragment.java
+++ app/src/main/java/cn/njit/myapplication/fragment/nearfragment/CustomFragment.java
@@ -1,11 +1,12 @@
 package cn.njit.myapplication.fragment.nearfragment;
 
-import android.graphics.Color;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
-import android.widget.TextView;
 
+import com.amap.api.maps2d.AMap;
+import com.amap.api.maps2d.MapView;
+
+import cn.njit.myapplication.R;
 import cn.njit.myapplication.fragment.BaseFragment;
 
 
@@ -13,22 +14,22 @@ public class CustomFragment extends BaseFragment {
 
 
     private static final String TAG = CustomFragment.class.getSimpleName();//"CommonFrameFragment"
-    private TextView textView;
 
     @Override
     protected View initView() {
-        Log.e(TAG,"自定义Fragment页面被初始化了...");
-        textView = new TextView(mContext);
-        textView.setTextSize(20);
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextColor(Color.RED);
-        return textView;
+        Log.e(TAG, "自定义Fragment页面被初始化了...");
+        View view = View.inflate(mContext, R.layout.fragment_custom_fragment, null);
+        MapView mapView = view.findViewById(R.id.map);//找到地图控件
+//在activity执行onCreate时执行mMapView.onCreate(savedInstanceState)，创建地图
+        mapView.onCreate(getArguments());
+        AMap aMap = mapView.getMap();//初始化地图控制器对象
+
+        return view;
     }
 
     @Override
     protected void initData() {
         super.initData();
         Log.e(TAG, "自定义Fragment数据被初始化了...");
-        textView.setText("自定义页面");
     }
 }
